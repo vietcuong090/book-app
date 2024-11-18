0e08dcd2ab6ddabd7a4b443adc5d64e9b953efe6
vietcuong090 <tranvietcuong090@gmail.com>
2024-11-18 10:37:15 +0700

    commit frontend-backend

---
+++ backend/src/books/book.route.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const Book = require('./book.model');
+const { postABook, getAllBooks, getSingleBook, UpdateBook, deleteABook } = require('./book.controller');
+const verifyAdminToken = require('../middleware/verifyAdminToken');
+const router = express.Router();
+
+// frontend => backend server => controller => book schema  => database => send to server => back to the frontend
+//post = when submit something fronted to db
+// get =  when get something back from db
+// put/patch = when edit or update something
+// delete = when delete something
+
+// post a book
+router.post('/create-book', verifyAdminToken, postABook);
+// router.post('/create-book', async (req, res) => {
+//   try {
+//     const newBook = await Book({ ...req.body });
+//     await newBook.save();
+//     res.status(200).send({ message: 'Book posted successfully ', book: newBook });
+//   } catch (error) {
+//     console.error('Error creating book', error);
+//     res.status(500).send({ message: 'Failed to create booke' });
+//   }
+// });
+
+// get all books
+router.get('/', getAllBooks);
+
+// single book endpoint
+router.get('/:id', getSingleBook);
+
+// update a book endpoint
+router.put('/edit/:id', verifyAdminToken, UpdateBook);
+
+router.delete('/:id', verifyAdminToken, deleteABook);
+
+module.exports = router;
