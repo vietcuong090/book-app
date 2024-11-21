ad70de17a25c39afb0cf84bac8bf9fb0879025c0
vietcuong090 <tranvietcuong090@gmail.com>
2024-11-21 20:04:55 +0700

    update cors

--- backend/index.js
+++ backend/index.js
@@ -10,7 +10,11 @@ require('dotenv').config();
 app.use(express.json());
 app.use(
   cors({
-    origin: ['http://localhost:5173', 'https://book-app-store-fco37ve5q-cuongs-projects-c50ae558.vercel.app'],
+    origin: [
+      'http://localhost:5173',
+      'https://book-app-store-fco37ve5q-cuongs-projects-c50ae558.vercel.app',
+      'https://book-app-lake-seven.vercel.app',
+    ],
     credentials: true,
   })
 );
